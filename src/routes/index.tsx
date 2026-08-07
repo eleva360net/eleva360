f069c3b7d2ed2fa8704df20c0e998913bc5fd592
Carlos <eleva360.net@gmail.com>
2026-08-06 23:16:44 -0400

    Aumentar tamaño del logo

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -311,7 +311,7 @@ function Logo() {
       <img
         src={logoEleva360}
         alt="Eleva360"
-        className="h-10 w-auto transition-transform duration-300 group-hover:scale-[1.02]"
+        className="h-14 w-auto transition-transform duration-300 group-hover:scale-[1.02]"
       />
     </Link>
   );
